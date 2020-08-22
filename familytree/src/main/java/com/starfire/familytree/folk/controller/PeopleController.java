f8429ba58bb9d36178342ef9f43b53039858b103
Administrator <nysyxxg@163.com>
2020-08-22 12:24:21 +0800

    族谱树

--- familytree/src/main/java/com/starfire/familytree/folk/controller/PeopleController.java
+++ familytree/src/main/java/com/starfire/familytree/folk/controller/PeopleController.java
@@ -34,249 +34,247 @@ import java.util.*;
 @Api(tags = "人物模块")
 public class PeopleController {
 
-    @Autowired
-    private IPeopleService peopleService;
+	@Autowired
+	private IPeopleService peopleService;
 
-    @Autowired
-    private IPartnerService partnerService;
+	@Autowired
+	private IPartnerService partnerService;
 
-    @Autowired
-    private IChildrenService childrenService;
+	@Autowired
+	private IChildrenService childrenService;
 
-    /**
-     * 分页
-     *
-     * @param page
-     * @return
-     * @author luzh
-     */
-    @PostMapping("/page")
-    public PageInfo<Map<String, Object>, People> page(@RequestBody(required = false) PageInfo<Map<String, Object>, People> page) {
-        page = page == null ? new PageInfo<>() : page;
-        PageInfo<Map<String, Object>, People> pageInfo = peopleService.page(page);
-        return pageInfo;
+	/**
+	 * 分页
+	 *
+	 * @param page
+	 * @return
+	 * @author luzh
+	 */
+	@PostMapping("/page")
+	public PageInfo<Map<String, Object>, People> page(@RequestBody(required = false) PageInfo<Map<String, Object>, People> page) {
+		page = page == null ? new PageInfo<>() : page;
+		PageInfo<Map<String, Object>, People> pageInfo = peopleService.page(page);
+		return pageInfo;
 
-    }
-
-
-    @PostMapping("add")
-    public People addPeople(@RequestBody @Valid People people) {
-        People pl = peopleService.addPeople(people);
-        return pl;
-    }
-
-    @GetMapping("get_peoples_by_generation")
-    public List<People> getPeoplesByGeneration(@RequestParam(required = true) int gen) {
-        List<People> peoples = peopleService.getPeoplesByGeneration(gen);
-        return peoples;
-    }
-
-    @GetMapping("getPeopleByName")
-    public List<People> getPeopleByName(@RequestParam(required = true) String name) {
-        List<People> peoples = peopleService.getPeopleByName(name);
-        return peoples;
-    }
-
-    @GetMapping("/get")
-    public People getPeople(Long id) {
-        People people = peopleService.getById(id);
-        return people;
-    }   
-    
-    @GetMapping("/view")
-    public People viewPeople(Long id) {
-        People people = peopleService.getPeople(id);
-        return people;
-    }
-
-    @PostMapping("/delete")
-    public Boolean deletePeople(@RequestBody DeleteVO<Long[]> deleteVO) {
-        Long[] ids = deleteVO.getIds();
-        for (int i = 0; i < ids.length; i++) {
-            Long id = Long.valueOf(ids[i]);
-            boolean b = peopleService.removeById(id);
-
-        }
-        return true;
-    }
-
-    @PostMapping("/edit")
-    public Boolean editPeople(@RequestBody People people) {
-        String pinyin = StringHelper.toPinyin(people.getFullName());
-        people.setPinyin(pinyin);
-        boolean b = peopleService.updateById(people);
-        return b;
-    }
-
-    @PostMapping("/addRelationship")
-    public Boolean addRelationship(@RequestBody RelationshipVO relationshipVO) {
-        boolean b = peopleService.addRelationship(relationshipVO);
-        return b;
-    }
-
-    @PostMapping("/getNames")
-    public List<Map<String,Object>> getNames(@RequestBody Map<String,String> param) {
-        List<Map<String,Object>> names=new ArrayList<>();
-        String name = param.get("name");
-        if(name.length()>1) {
-
-        names = peopleService.getNames(name);
-        }
-        return names;
-    }
-    @GetMapping("/test")
-    public void test() {
-        List<People> list = peopleService.list();
-        for (int i = 0; i < list.size(); i++) {
-            People people =  list.get(i);
-            String pinyin = StringHelper.toPinyin(people.getFullName());
-            people.setPinyin(pinyin);
-            peopleService.saveOrUpdate(people);
-        }
-    }
-
-    @PostMapping("/tree")
-    public OrgChartVO tree(@RequestBody Map<String,Integer> param) {
-        OrgChartVO orgChartVO = new OrgChartVO();
-        People husband = peopleService.getForefather(param.get("gen"));
-        Long fatherId = husband.getId();
-        Long husbandId = husband.getId();
-        //获取妻子
-        People wife = partnerService.getWife(husbandId);
-        if(wife!=null){
-            OrgChartItemVO orgChartItemVO = convertOrgChartItemVO(husbandId,wife);
-            orgChartVO.getItems().add(orgChartItemVO);
-        }
-        loopChildren(orgChartVO, husband,wife);
-        String fullName = husband.getFullName();
-        String brief = husband.getBrief();
-        String avatar = husband.getAvatar();
-        OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
-        orgChartItemVO.setId(Math.abs(fatherId.hashCode()));
-        orgChartItemVO.setPeopleId(fatherId+"");
-        orgChartItemVO.setParents(null);
-        orgChartItemVO.setTitle(fullName);
-        orgChartItemVO.setDescription(brief);
-        orgChartItemVO.setImage(avatar);
-        orgChartVO.getItems().add(orgChartItemVO);
-        return orgChartVO;
-    }
-
-    @PostMapping("/getFamilyTree")
-    public OrgChartVO getFamilyTree(@RequestBody Map<String,String> param) {
-        OrgChartVO orgChartVO = new OrgChartVO();
-
-        People husband = peopleService.getFamilyTree(param);
-        Integer generations = husband.getGenerations();
-        Long fatherId = husband.getId();
-        Long husbandId = husband.getId();
-        //获取妻子
-        People wife = partnerService.getWife(husbandId);
-        if(wife!=null){
-            OrgChartItemVO orgChartItemVO = convertOrgChartItemVO(husbandId,wife);
-            String avatar = wife.getAvatar();
-            String brief = wife.getBrief();
-            GenderEnum gender = wife.getGender();
-            String sex = gender.name();
-            orgChartItemVO.setSex(sex);
-            orgChartItemVO.setPeopleId(wife.getId()+"");
-            orgChartItemVO.setGenerations("第"+ ChineseNumber.numberToCH(generations)+"世");
-            orgChartItemVO.setDescription(brief);
-            orgChartItemVO.setRemark(wife.getRemark());
-            orgChartVO.getItems().add(orgChartItemVO);
-        }
-        loopChildren(orgChartVO, husband,wife);
-        String fullName = husband.getFullName();
-        String brief = husband.getBrief();
-        GenderEnum gender = husband.getGender();
-        String sex = gender.name();
-        OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
-        orgChartItemVO.setId(Math.abs(fatherId.hashCode()));
-        orgChartItemVO.setParents(null);
-        orgChartItemVO.setPeopleId(fatherId+"");
-        orgChartItemVO.setTitle(fullName);
-        orgChartItemVO.setSex(sex);
-        orgChartItemVO.setGenerations("第"+ ChineseNumber.numberToCH(generations)+"世");
-        orgChartItemVO.setDescription(brief);
-        orgChartItemVO.setRemark(husband.getRemark());
-        orgChartVO.getItems().add(orgChartItemVO);
-        return orgChartVO;
-    }
-
-    /**
-     * 轮询取出所有后代
-     * @param orgChartVO
-     */
-    private void loopChildren(OrgChartVO orgChartVO, People father,People mother) {
-        Long fatherId=father.getId();
-        Long motherId=null;
-        if(mother!=null){
-            motherId=mother.getId();
-        }
-        List<People> childrenList = childrenService.getChildrenList(fatherId);
-        for (int j = 0; j < childrenList.size(); j++) {
-            People children = childrenList.get(j);
-            String fullName = children.getFullName();
-            String brief = children.getBrief();
-            GenderEnum gender = children.getGender();
-
-            String sex = gender.name();
-            Long childrenId = children.getId();
-            //获取妻子
-            People wife = partnerService.getWife(childrenId);
-            if(wife!=null){
-                OrgChartItemVO orgChartItemVO = convertOrgChartItemVO(childrenId,wife);
-                String wifeBrief = wife.getBrief();
-                GenderEnum wifeGender = wife.getGender();
-               Integer generations = wife.getGenerations();
-                String wifesex = wifeGender.name();
-                orgChartItemVO.setSex(wifesex);
-                orgChartItemVO.setPeopleId(wife.getId()+"");
-                orgChartItemVO.setGenerations("第"+ ChineseNumber.numberToCH(generations)+"世");
-                orgChartItemVO.setDescription(wifeBrief);
-                orgChartItemVO.setRemark(wife.getRemark());
-                orgChartVO.getItems().add(orgChartItemVO);
-            }
-            //获取孩子
-            OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
-            orgChartItemVO.setId(Math.abs(childrenId.hashCode()));
-            orgChartItemVO.setPeopleId(childrenId+"");
-            Integer[] parents=new Integer[2];
-            parents[0]=Math.abs(fatherId.hashCode());
-            if(motherId!=null){
-
-            parents[1]=Math.abs(motherId.hashCode());
-            }
-            Integer generations = children.getGenerations();
-            orgChartItemVO.setPeopleId(children.getId()+"");
-            orgChartItemVO.setParents(parents);
-            orgChartItemVO.setTitle(fullName);
-            orgChartItemVO.setLabel(children.getHeir());
-            orgChartItemVO.setSex(sex);
-            orgChartItemVO.setGenerations("第"+ ChineseNumber.numberToCH(generations)+"世");
-            orgChartItemVO.setDescription(brief);
-            orgChartItemVO.setRemark(children.getRemark());
-            orgChartVO.getItems().add(orgChartItemVO);
-            loopChildren(orgChartVO,children,wife);
-        }
-    }
+	}
 
 
-    private OrgChartItemVO convertOrgChartItemVO(Long husbandId,People wife) {
-        String fullName = wife.getFullName();
-        String brief = wife.getBrief();
-        OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
-        orgChartItemVO.setId(Math.abs(wife.getId().hashCode()));
-        orgChartItemVO.setParents(null);
-        orgChartItemVO.setPeopleId(wife.getId()+"");
-        orgChartItemVO.setTitle(fullName);
-        orgChartItemVO.setDescription(brief);
-        orgChartItemVO.setGenerations("第"+ ChineseNumber.numberToCH(wife.getGenerations())+"世");
-        orgChartItemVO.setPosition(Math.abs(husbandId.hashCode()+1));
-        orgChartItemVO.setRelativeItem(Math.abs(husbandId.hashCode()));
+	@PostMapping("add")
+	public People addPeople(@RequestBody @Valid People people) {
+		People pl = peopleService.addPeople(people);
+		return pl;
+	}
 
+	@GetMapping("get_peoples_by_generation")
+	public List<People> getPeoplesByGeneration(@RequestParam(required = true) int gen) {
+		List<People> peoples = peopleService.getPeoplesByGeneration(gen);
+		return peoples;
+	}
 
-        return orgChartItemVO;
-    }
+	@GetMapping("getPeopleByName")
+	public List<People> getPeopleByName(@RequestParam(required = true) String name) {
+		List<People> peoples = peopleService.getPeopleByName(name);
+		return peoples;
+	}
+
+	@GetMapping("/get")
+	public People getPeople(Long id) {
+		People people = peopleService.getById(id);
+		return people;
+	}
+
+	@GetMapping("/view")
+	public People viewPeople(Long id) {
+		People people = peopleService.getPeople(id);
+		return people;
+	}
+
+	@PostMapping("/delete")
+	public Boolean deletePeople(@RequestBody DeleteVO<Long[]> deleteVO) {
+		Long[] ids = deleteVO.getIds();
+		for (int i = 0; i < ids.length; i++) {
+			Long id = Long.valueOf(ids[i]);
+			boolean b = peopleService.removeById(id);
+
+		}
+		return true;
+	}
+
+	@PostMapping("/edit")
+	public Boolean editPeople(@RequestBody People people) {
+		String pinyin = StringHelper.toPinyin(people.getFullName());
+		people.setPinyin(pinyin);
+		boolean b = peopleService.updateById(people);
+		return b;
+	}
+
+	@PostMapping("/addRelationship")
+	public Boolean addRelationship(@RequestBody RelationshipVO relationshipVO) {
+		boolean b = peopleService.addRelationship(relationshipVO);
+		return b;
+	}
+
+	@PostMapping("/getNames")
+	public List<Map<String, Object>> getNames(@RequestBody Map<String, String> param) {
+		List<Map<String, Object>> names = new ArrayList<>();
+		String name = param.get("name");
+		if (name.length() > 1) {
+
+			names = peopleService.getNames(name);
+		}
+		return names;
+	}
+
+	@GetMapping("/test")
+	public void test() {
+		List<People> list = peopleService.list();
+		for (int i = 0; i < list.size(); i++) {
+			People people = list.get(i);
+			String pinyin = StringHelper.toPinyin(people.getFullName());
+			people.setPinyin(pinyin);
+			peopleService.saveOrUpdate(people);
+		}
+	}
+
+	@PostMapping("/tree")
+	public OrgChartVO tree(@RequestBody Map<String, Integer> param) {
+		OrgChartVO orgChartVO = new OrgChartVO();
+		People husband = peopleService.getForefather(param.get("gen"));
+		Long fatherId = husband.getId();
+		Long husbandId = husband.getId();
+		//获取妻子
+		People wife = partnerService.getWife(husbandId);
+		if (wife != null) {
+			OrgChartItemVO orgChartItemVO = convertOrgChartItemVO(husbandId, wife);
+			orgChartVO.getItems().add(orgChartItemVO);
+		}
+		loopChildren(orgChartVO, husband, wife);
+		String fullName = husband.getFullName();
+		String brief = husband.getBrief();
+		String avatar = husband.getAvatar();
+		OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
+		orgChartItemVO.setId(Math.abs(fatherId.hashCode()));
+		orgChartItemVO.setPeopleId(fatherId + "");
+		orgChartItemVO.setParents(null);
+		orgChartItemVO.setTitle(fullName);
+		orgChartItemVO.setDescription(brief);
+		orgChartItemVO.setImage(avatar);
+		orgChartVO.getItems().add(orgChartItemVO);
+		return orgChartVO;
+	}
+
+	@PostMapping("/getFamilyTree")
+	public OrgChartVO getFamilyTree(@RequestBody Map<String, String> param) {
+		OrgChartVO orgChartVO = new OrgChartVO();
+
+		People husband = peopleService.getFamilyTree(param);// 获取丈夫
+		Integer generations = husband.getGenerations();
+		Long fatherId = husband.getId();// 丈夫的父亲Id
+		Long husbandId = husband.getId(); // 丈夫id
+		//获取妻子
+		People wife = partnerService.getWife(husbandId);
+		if (wife != null) {
+			OrgChartItemVO orgChartItemVO = convertOrgChartItemVO(husbandId, wife);// 对数据进行封装转换
+			String avatar = wife.getAvatar();
+			String brief = wife.getBrief();
+			GenderEnum gender = wife.getGender();// 得到性别
+			String sex = gender.name();
+			orgChartItemVO.setSex(sex);
+			orgChartItemVO.setPeopleId(wife.getId() + "");
+			orgChartItemVO.setGenerations("第" + ChineseNumber.numberToCH(generations) + "世");
+			orgChartItemVO.setDescription(brief);
+			orgChartItemVO.setRemark(wife.getRemark());
+			orgChartVO.getItems().add(orgChartItemVO);
+		}
+		loopChildren(orgChartVO, husband, wife);// 获取妻子和丈夫的孩子，数据存储在orgChartVO对象中
+		String fullName = husband.getFullName();
+		String brief = husband.getBrief();
+		GenderEnum gender = husband.getGender();
+		String sex = gender.name();
+		OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
+		orgChartItemVO.setId(Math.abs(fatherId.hashCode()));
+		orgChartItemVO.setParents(null);
+		orgChartItemVO.setPeopleId(fatherId + "");
+		orgChartItemVO.setTitle(fullName);
+		orgChartItemVO.setSex(sex);
+		orgChartItemVO.setGenerations("第" + ChineseNumber.numberToCH(generations) + "世");
+		orgChartItemVO.setDescription(brief);
+		orgChartItemVO.setRemark(husband.getRemark());
+		orgChartVO.getItems().add(orgChartItemVO);
+		return orgChartVO;
+	}
+
+	/**
+	 * 轮询取出所有后代
+	 * @param orgChartVO
+	 */
+	private void loopChildren(OrgChartVO orgChartVO, People father, People mother) {
+		Long fatherId = father.getId();
+		Long motherId = null;
+		if (mother != null) {
+			motherId = mother.getId();
+		}
+		List<People> childrenList = childrenService.getChildrenList(fatherId);
+		for (int j = 0; j < childrenList.size(); j++) {//只要存在孩子后代，就不断的轮询查询数据
+			People children = childrenList.get(j);
+			String fullName = children.getFullName();
+			String brief = children.getBrief();
+			GenderEnum gender = children.getGender();
+
+			String sex = gender.name();
+			Long childrenId = children.getId();
+			//获取妻子
+			People wife = partnerService.getWife(childrenId);
+			if (wife != null) {
+				OrgChartItemVO orgChartItemVO = convertOrgChartItemVO(childrenId, wife);
+				String wifeBrief = wife.getBrief();
+				GenderEnum wifeGender = wife.getGender();
+				Integer generations = wife.getGenerations();
+				String wifesex = wifeGender.name();
+				orgChartItemVO.setSex(wifesex);
+				orgChartItemVO.setPeopleId(wife.getId() + "");
+				orgChartItemVO.setGenerations("第" + ChineseNumber.numberToCH(generations) + "世");
+				orgChartItemVO.setDescription(wifeBrief);
+				orgChartItemVO.setRemark(wife.getRemark());
+				orgChartVO.getItems().add(orgChartItemVO);
+			}
+			//获取孩子
+			OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
+			orgChartItemVO.setId(Math.abs(childrenId.hashCode()));
+			orgChartItemVO.setPeopleId(childrenId + "");
+			Integer[] parents = new Integer[2];
+			parents[0] = Math.abs(fatherId.hashCode());
+			if (motherId != null) {
+				parents[1] = Math.abs(motherId.hashCode());
+			}
+			Integer generations = children.getGenerations();
+			orgChartItemVO.setPeopleId(children.getId() + "");
+			orgChartItemVO.setParents(parents);
+			orgChartItemVO.setTitle(fullName);
+			orgChartItemVO.setLabel(children.getHeir());
+			orgChartItemVO.setSex(sex);
+			orgChartItemVO.setGenerations("第" + ChineseNumber.numberToCH(generations) + "世");
+			orgChartItemVO.setDescription(brief);
+			orgChartItemVO.setRemark(children.getRemark());
+			orgChartVO.getItems().add(orgChartItemVO);
+			loopChildren(orgChartVO, children, wife);
+		}
+	}
+
+	///对数据进行封装转换
+	private OrgChartItemVO convertOrgChartItemVO(Long husbandId, People wife) {
+		String fullName = wife.getFullName();
+		String brief = wife.getBrief();
+		OrgChartItemVO orgChartItemVO = new OrgChartItemVO();
+		orgChartItemVO.setId(Math.abs(wife.getId().hashCode()));
+		orgChartItemVO.setParents(null);
+		orgChartItemVO.setPeopleId(wife.getId() + "");
+		orgChartItemVO.setTitle(fullName);
+		orgChartItemVO.setDescription(brief);
+		orgChartItemVO.setGenerations("第" + ChineseNumber.numberToCH(wife.getGenerations()) + "世");
+		orgChartItemVO.setPosition(Math.abs(husbandId.hashCode() + 1));
+		orgChartItemVO.setRelativeItem(Math.abs(husbandId.hashCode()));
+		return orgChartItemVO;
+	}
 
 }
--- familytree/src/main/java/com/starfire/familytree/folk/entity/People.java
+++ familytree/src/main/java/com/starfire/familytree/folk/entity/People.java
@@ -30,148 +30,149 @@ import java.util.Date;
 @TableName("bs_people")
 public class People extends AbstractEntity {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 
-    /**
-     * 头像
-     */
-    private String avatar;
+	/**
+	 * 头像
+	 */
+	private String avatar;
 
-    /**
-     * 小名
-     */
-    private String nickname;
+	/**
+	 * 小名
+	 */
+	private String nickname;
 
-    /**
-     * 全名
-     */
-    @NotNull(message = "全名不能为空")
-    private String fullName;
+	/**
+	 * 全名
+	 */
+	@NotNull(message = "全名不能为空")
+	private String fullName;
 
-    /**
-     * 全名拼音
-     */
-    private String pinyin;
+	/**
+	 * 全名拼音
+	 */
+	private String pinyin;
 
-    /**
-     * 入嗣
-     */
-    private String heir;
+	/**
+	 * 入嗣
+	 */
+	private String heir;
 
-    /**
-     * 性别
-     */
-    @NotNull(message = "性别不能为空")
-    private GenderEnum gender;
+	/**
+	 * 性别
+	 */
+	@NotNull(message = "性别不能为空")
+	private GenderEnum gender;
 
-    /**
-     * 年龄
-     */
-    private Integer age;
+	/**
+	 * 年龄
+	 */
+	private Integer age;
 
-    /**
-     * 是否已婚
-     */
-    private BooleanEnum isMarried=BooleanEnum.否;
+	/**
+	 * 是否已婚
+	 */
+	private BooleanEnum isMarried = BooleanEnum.否;
 
-    /**
-     * 是否有后代
-     */
-    private BooleanEnum hasChild=BooleanEnum.否;
+	/**
+	 * 是否有后代
+	 */
+	private BooleanEnum hasChild = BooleanEnum.否;
 
-    /**
-     * 第几世
-     */
-    @Positive(message = "第几世要大于0")
-    private Integer generations=1;
+	/**
+	 * 第几世
+	 */
+	@Positive(message = "第几世要大于0")
+	private Integer generations = 1;
 
-    @TableField(exist = false)
-    private String generationsText;
+	@TableField(exist = false)
+	private String generationsText;
 
-    /**
-     * 生日
-     */
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date birth;
+	/**
+	 * 生日
+	 */
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date birth;
 
-    /**
-     * 逝期
-     */
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date death;
+	/**
+	 * 逝期
+	 */
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date death;
 
-    /**
-     * 备注
-     */
-    private String remark;
+	/**
+	 * 备注
+	 */
+	private String remark;
 
-    /**
-     * 个人描述
-     */
-    private String brief;
+	/**
+	 * 个人描述
+	 */
+	private String brief;
 
-    /**
-     * 教育
-     */
-    private String education;
+	/**
+	 * 教育
+	 */
+	private String education;
 
-    /**
-     * 身高
-     */
-    private String height;
+	/**
+	 * 身高
+	 */
+	private String height;
 
-    /**
-     * 体重
-     */
-    private String weight;
-
-
-    /**
-     * 手机号码
-     */
-    private String phoneNumber;
-
-    /**
-     * 工作单位
-     */
-    private String company;
-
-    /**
-     * 职务
-     */
-    private String job;
-
-    /**
-     * 分支 关联category
-     */
-    @JsonSerialize(using = ToStringSerializer.class)
-    private Long peopleBranch;
+	/**
+	 * 体重
+	 */
+	private String weight;
 
 
-    @TableField(exist = false)
-    private String branchName;
+	/**
+	 * 手机号码
+	 */
+	private String phoneNumber;
 
-    /**
-     * 生产队
-     */
-    @JsonSerialize(using = ToStringSerializer.class)
-    private Long prodTeam;
+	/**
+	 * 工作单位
+	 */
+	private String company;
 
-  /**
-   * @title 生产队名称
-   */
-    @TableField(exist = false)
-    private String prodTeamName;
+	/**
+	 * 职务
+	 */
+	private String job;
 
-    /**
-     * 享年
-     */
-    @TableField(exist = false)
-    private Integer aliveAge;
+	/**
+	 * 分支 关联category
+	 */
+	@JsonSerialize(using = ToStringSerializer.class)
+	private Long peopleBranch;
 
-    /** 关联乡村
-     */
-    private String villageCode;
+
+	@TableField(exist = false)
+	private String branchName;
+
+	/**
+	 * 生产队
+	 */
+	@JsonSerialize(using = ToStringSerializer.class)
+	private Long prodTeam;
+
+	/**
+	 * @title 生产队名称
+	 */
+	@TableField(exist = false)
+	private String prodTeamName;
+
+	/**
+	 * 享年
+	 */
+	@TableField(exist = false)
+	private Integer aliveAge;
+
+	/**
+	 * 关联乡村
+	 */
+	private String villageCode;
 
 }
